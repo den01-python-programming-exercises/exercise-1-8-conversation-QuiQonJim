065379dfebfa27e6956ff59b169e7ef4d69ea796
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-01 13:39:50 +0100

    attempt 3

--- src/exercise.py
+++ src/exercise.py
@@ -3,7 +3,7 @@ def main():
     print(first)#write your code below this line
     second = input('Oh, how interestig. Tell me more!')
     print(second)
-    print = ('Thanks for sharing!'')
+    print = ('Thanks for sharing!')
 
 
 if __name__ == '__main__':
