03eb6651490beeee78750128d275bd55e3cd52ed
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-01 13:45:30 +0100

    attempt 4

--- src/exercise.py
+++ src/exercise.py
@@ -3,8 +3,8 @@ def main():
     print('*'+first+'*')#write your code below this line
     second = input('Oh, how interestig. Tell me more!')
     print('*' +second +'*')
-    third = input('Thanks for sharing!')
-    
+    print('Thanks for sharing!')
+
 
 
 if __name__ == '__main__':
