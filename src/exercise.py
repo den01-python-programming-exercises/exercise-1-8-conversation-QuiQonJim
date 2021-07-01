bd23c5488b0e95add9946c38526b5895dd57c1aa
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-01 13:44:13 +0100

    attempt 4

--- src/exercise.py
+++ src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     first = input('Greetings! How are you doing?')
-    print(first)#write your code below this line
+    print('*'+first+'*')#write your code below this line
     second = input('Oh, how interestig. Tell me more!')
-    print(second)
-    print = ('Thanks for sharing!')
+    print('*' +second +'*')
+    third = input('Thanks for sharing!')
+    
 
 
 if __name__ == '__main__':
